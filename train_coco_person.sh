396fd35024f69ad8cc19be7549819ee9633f7091
zhenghe <zhenghe2009@gmail.com>
2017-06-16 16:57:44 +0800

    Update train_coco_person.sh

--- train_coco_person.sh
+++ train_coco_person.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-outpath=/home/store-1-img/zhenghe/flickr8kcn/im2txt_for_flick_log.csv
+outpath=/home/store-1-img/zhenghe/coco_person/im2txt_for_coco_person_log.csv
 
-cd /home/store-1-img/zhenghe/models/im2txt_for_flick
-FLICK_DIR="/home/store-1-img/zhenghe/flickr8kcn/TFRECORD_data"
+cd /home/store-1-img/zhenghe/models/im2txt
+COCO_PERSON_DIR="/home/store-1-img/zhenghe/coco_person/TFRECORD_data"
 INCEPTION_CHECKPOINT="/home/store-1-img/zhenghe/im2txt/data/inception_v3.ckpt"
-MODEL_DIR="/home/store-1-img/zhenghe/flickr8kcn/model"
+MODEL_DIR="/home/store-1-img/zhenghe/coco_person/model"
 bazel build -c opt im2txt/...
 export CUDA_VISIBLE_DEVICES="2"
 bazel-bin/im2txt/train \
-  --input_file_pattern="${FLICK_DIR}/train-?????-of-00008" \
+  --input_file_pattern="${COCO_PERSON_DIR}/train-?????-of-00032" \
   --inception_checkpoint_file="${INCEPTION_CHECKPOINT}" \
   --train_dir="${MODEL_DIR}/train" \
   --train_inception=false \
-  --number_of_steps=1000000 > ${outpath} 2>&1 &
+  --number_of_steps=100000 > ${outpath} 2>&1 &
