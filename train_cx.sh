41aefdf523f2e75bdca026e94f8bff8f09a5982a
zhhezhhe <zhenghe2009@gmail.com>
2017-07-27 12:53:51 +0800

    train_cx for 100000

--- train_cx.sh
+++ train_cx.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-outpath=/home/store-1-img/zhenghe/chuangxin_data/im2txt_for_chuangxin_log.csv
+outpath=/home/store-1-img/zhenghe/chuangxin_data/im2txt_for_chuangxin_log_100000.csv
 
 cd /home/store-1-img/zhenghe/models/im2txt_for_chuangxin
-CHUANGXIN_DIR="/home/store-1-img/zhenghe/caption_faceplusplus/chuangxinTFRECORD_data"
+CHUANGXIN_DIR="/home/store-1-img/zhenghe/caption_faceplusplus/chuangxinTFRECORD_data_100000"
 INCEPTION_CHECKPOINT="/home/store-1-img/zhenghe/im2txt/data/inception_v3.ckpt"
-MODEL_DIR="/home/store-1-img/zhenghe/chuangxin_data/model"
+MODEL_DIR="/home/store-1-img/zhenghe/chuangxin_data/model_100000"
 bazel build -c opt im2txt/...
 export CUDA_VISIBLE_DEVICES="2"
 bazel-bin/im2txt/train \
