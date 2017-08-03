3ea59fb3dc54c2672174a746873d969ed9e6c653
zhhezhhe <zhenghe2009@gmail.com>
2017-08-03 17:50:00 +0800

    change file for test

--- train_cx_finetune.sh
+++ train_cx_finetune.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-outpath=/home/store-1-img/zhenghe/chuangxin_data/im2txt_for_chuangxin_finetune_log.csv
+outpath=/home/store-1-img/zhenghe/chuangxin_data/im2txt_for_chuangxin_finetune_log_100000.csv
 cd /home/store-1-img/zhenghe/models/im2txt_for_chuangxin
-CHUANGXIN_DIR="/home/store-1-img/zhenghe/caption_faceplusplus/chuangxinTFRECORD_data"
+CHUANGXIN_DIR="/home/store-1-img/zhenghe/caption_faceplusplus/chuangxinTFRECORD_data_100000"
 INCEPTION_CHECKPOINT="/home/store-1-img/zhenghe/im2txt/data/inception_v3.ckpt"
-MODEL_DIR="/home/store-1-img/zhenghe/chuangxin_data/model"
+MODEL_DIR="/home/store-1-img/zhenghe/chuangxin_data/model_100000"
 bazel build -c opt im2txt/...
-export CUDA_VISIBLE_DEVICES="2"
+export CUDA_VISIBLE_DEVICES="0"
 bazel-bin/im2txt/train \
-  --input_file_pattern="${CHUANGXIN_DIR}/train-?????-of-00224" \
+  --input_file_pattern="${CHUANGXIN_DIR}/train-?????-of-00280" \
   --train_dir="${MODEL_DIR}/train" \
   --train_inception=true \
   --number_of_steps=3000000 > ${outpath} 2>&1 &
